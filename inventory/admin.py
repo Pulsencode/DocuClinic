b262e22e11c3ad099186988f74b69a85a14de7b2
abhimanue <abhimanuemvk@gmail.com>
2024-10-29 15:30:46 +0530

    added the prescription module

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Supplier, Medicine, DosageForm, MedicineSupplier
+from .models import Supplier, Medicine, RouteOfAdministration, MedicineSupplier, Prescription, PrescriptionMedicine
 
 admin.site.register(Supplier)
 admin.site.register(Medicine)
-admin.site.register(DosageForm)
+admin.site.register(RouteOfAdministration)
 admin.site.register(MedicineSupplier)
+admin.site.register(Prescription)
+admin.site.register(PrescriptionMedicine)
--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from accounts.models import Patient, Doctor
 
 
 class Supplier(models.Model):
@@ -12,7 +13,9 @@ class Supplier(models.Model):
         return self.name
 
 
-class DosageForm(models.Model):
+class RouteOfAdministration(models.Model):
+    """This model determines the way the medicine is taken. example oral, injection etc.."""
+
     name = models.CharField(max_length=255)
     route_of_administration = models.CharField(max_length=255)
 
@@ -24,10 +27,12 @@ class Medicine(models.Model):
     name = models.CharField(max_length=255)
     generic_name = models.CharField(max_length=255)
     brand_name = models.CharField(max_length=255)
-    dosage_form = models.ForeignKey(
-        DosageForm, on_delete=models.SET_NULL, null=True, related_name="dosageForm"
+    route_of_administration = models.ForeignKey(
+        RouteOfAdministration,
+        on_delete=models.SET_NULL,
+        null=True,
     )
-    dosage_strength = models.CharField(max_length=255)
+    # dosage_strength = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     quantity = models.PositiveIntegerField()
     expiration_date = models.DateField()
@@ -51,3 +56,36 @@ class MedicineSupplier(models.Model):
 
     def __str__(self):
         return f"{self.supplier.name} - {self.medicine.name} at {self.price}"
+
+
+class Prescription(models.Model):
+    """Ividay adhyam prescription create akkumm anit prescription medicine model vech medicine add akkaam"""
+    patient = models.ForeignKey(
+        Patient, on_delete=models.CASCADE, related_name="prescriptions"
+    )
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE
+    )
+    date_prescribed = models.DateTimeField(default=timezone.now)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Prescription for {self.patient.get_full_name()} by Dr. {self.doctor_name} on {self.date_prescribed}"
+
+
+class PrescriptionMedicine(models.Model):
+    """Appaam avasnaam print edukaan ith use akkaam, ith vech multiple medicine add akkaam"""
+    prescription = models.ForeignKey(
+        Prescription, on_delete=models.CASCADE, related_name="medicines"
+    )
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    dose = models.CharField(max_length=100)  # e.g., "500mg"
+    frequency = models.CharField(max_length=100)  # e.g., "Twice a day"
+    timing = models.CharField(max_length=255)  # e.g., "After meals"
+    amount = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )  # Optional for liquid or powder form
+    additional_instructions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.medicine.name} for {self.prescription.patient.get_full_name()} - {self.dose}, {self.frequency}"
