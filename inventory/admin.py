702ac71cf37b71201325677da1ceb077ddc3c5d1
abhimanue <abhimanuemvk@gmail.com>
2024-10-29 17:56:56 +0530

    moved models from inventory to doctor application

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Supplier, Medicine, RouteOfAdministration, MedicineSupplier, Prescription, PrescriptionMedicine
+from .models import Supplier, Medicine, RouteOfAdministration, MedicineSupplier
 
 admin.site.register(Supplier)
 admin.site.register(Medicine)
 admin.site.register(RouteOfAdministration)
 admin.site.register(MedicineSupplier)
-admin.site.register(Prescription)
-admin.site.register(PrescriptionMedicine)
