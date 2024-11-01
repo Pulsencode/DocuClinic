47aefba2617c6ba9be17321256ef6e65b7452d3e
abhimanue <abhimanuemvk@gmail.com>
2024-11-01 11:32:23 +0530

    migration changes and admin class adding

--- medicalrecords/admin.py
+++ medicalrecords/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-# from .models import Appointment
+from .models import Appointment, Prescription, PrescriptionMedicine
 
 
-# admin.site.register(Appointment)
+admin.site.register(Appointment)
+admin.site.register(Prescription)
+admin.site.register(PrescriptionMedicine)
