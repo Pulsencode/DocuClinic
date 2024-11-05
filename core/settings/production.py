8c600aa56419578e8830c35e741bd0c2632f9201
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2024-11-05 14:54:14 +0530

    docerized the application

--- core/settings/production.py
+++ core/settings/production.py
@@ -8,16 +8,23 @@ ALLOWED_HOSTS = [
 ]  # Example - Need to Replace with the server IP - Do not remove localhost
 
 # Database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.getenv("NAME"),  # noqa: F405
+#         "USER": os.getenv("USER"),  # noqa: F405
+#         "PASSWORD": os.getenv("PASSWORD"),  # noqa: F405
+#         "HOST": os.getenv("HOST"),  # noqa: F405
+#         "PORT": os.getenv("PORT"),  # noqa: F405
+#         "OPTIONS": {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("NAME"),  # noqa: F405
-        "USER": os.getenv("USER"),  # noqa: F405
-        "PASSWORD": os.getenv("PASSWORD"),  # noqa: F405
-        "HOST": os.getenv("HOST"),  # noqa: F405
-        "PORT": os.getenv("PORT"),  # noqa: F405
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
     }
 }
