0c64fce0aabb3afb3ddbf2ef06b1cea47ce0dea3
Alan-Anilkumar <alananilkumar125@gmail.com>
2024-11-08 10:27:15 +0530

    test commentend, removed sign up view

--- accounts/tests/test_render.py
+++ accounts/tests/test_render.py
@@ -1,21 +1,21 @@
-from django.test import TestCase
-from django.urls import reverse
+# from django.test import TestCase
+# from django.urls import reverse
 
 
-class SignUpPageRenderTestCase(TestCase):
-    def setUp(self):
-        self.response = self.client.get(reverse("signup"))
+# class SignUpPageRenderTestCase(TestCase):
+#     def setUp(self):
+#         self.response = self.client.get(reverse("signup"))
 
-    def test_signup_page_renders(self):
-        self.assertEqual(self.response.status_code, 200)
+#     def test_signup_page_renders(self):
+#         self.assertEqual(self.response.status_code, 200)
 
-    def test_page_content(self):
-        self.assertContains(self.response, 'name="username')
-        self.assertContains(self.response, 'name="email')
-        self.assertContains(self.response, 'name="phone_number')
-        self.assertContains(self.response, 'name="password1')
-        self.assertContains(self.response, 'name="password2')
+#     def test_page_content(self):
+#         self.assertContains(self.response, 'name="username')
+#         self.assertContains(self.response, 'name="email')
+#         self.assertContains(self.response, 'name="phone_number')
+#         self.assertContains(self.response, 'name="password1')
+#         self.assertContains(self.response, 'name="password2')
 
-    def test_page_template(self):
-        self.assertTemplateUsed(self.response, "registration/signup.html")
-        self.assertTemplateUsed(self.response, "general_base.html")
+#     def test_page_template(self):
+#         self.assertTemplateUsed(self.response, "registration/signup.html")
+#         self.assertTemplateUsed(self.response, "general_base.html")
